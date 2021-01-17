4c0e55c1a5609e8e7b752e8b18f1bd64bd806764
swatikp99 <swatikp99@gmail.com>
2021-01-17 14:28:07 +0530

    Minor Style Changes

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ $.getJSON("https://covid-api.mmediagroup.fr/v1/cases?country=India", function(da
     confirmCases=data.All.confirmed;
     recovered=data.All.recovered;
     deaths=data.All.deaths;
-    $('.covid-container .confirmed-cases').text("Confirmed Cases: "+confirmCases);
-    $('.recovered').text("Recovered: "+recovered);
-    $('.deaths').text("Deaths: "+deaths);
+    $('.confirmed-cases').append(confirmCases).css("color","blue");
+    $('.recovered').append(recovered).css("color","green");
+    $('.deaths').append(deaths).css("color","red");
 
     var count=1;
-    $('.container').append('<div class="row"><div class="state'+(count)+' col-md"><h3>State name</h3><p class="regionConfirm">Confirm</p><p class="regionRecovered">Recovered</p><p class="regionDeath">Death</p></div><div class="state'+(count+1)+' col-md"><h3>State name</h3><p class="regionConfirm">Confirm</p><p class="regionRecovered">Recovered</p><p class="regionDeath">Death</p></div><div class="state'+(count+2)+' col-md"><h3>State name</h3><p class="regionConfirm">Confirm</p><p class="regionRecovered">Recovered</p><p class="regionDeath">Death</p></div></div>'); 
+    $('.container').append('<div class="row"><div class="state'+(count)+' col-md"><h3>State name</h3><p>Confirmed Cases:&nbsp;<span class="regionConfirm"></span></p><p>Recovered:&nbsp;<span class="regionRecovered"></span></p><p>Deaths:&nbsp;<span class="regionDeath"></span></p></div><div class="state'+(count+1)+' col-md"><h3>State name</h3><p>Confirmed Cases:&nbsp;<span class="regionConfirm"></span></p><p>Recovered:&nbsp;<span class="regionRecovered"></span></p><p>Deaths:&nbsp;<span class="regionDeath"></span></p></div><div class="state'+(count+2)+' col-md"><h3>State name</h3><p>Confirmed Cases:&nbsp;<span class="regionConfirm"></span></p><p>Recovered:&nbsp;<span class="regionRecovered"></span></p><p>Deaths:&nbsp;<span class="regionDeath"></span></p></div></div>'); 
     $(document).ready(function(){
         for(var state in data){
           if(state=="Unknown")
@@ -16,11 +16,11 @@ $.getJSON("https://covid-api.mmediagroup.fr/v1/cases?country=India", function(da
           
           //$('.container').append('<div class="state'+(count)+' col-md"><h3>State name</h3><p class="regionConfirm">Confirm</p><p class="regionRecovered">Recovered</p><p class="regionDeath">Death</p></div>');
           $('.state'+count+' h3').text(state);
-          $('.state'+count+' .regionConfirm').text("Confirmed Cases: "+data[state].confirmed);
-          $('.state'+count+' .regionRecovered').text("Recovered: "+data[state].recovered);
-          $('.state'+count+' .regionDeath').text("Deaths: "+data[state].deaths);
+          $('.state'+count+' .regionConfirm').append(data[state].confirmed).css("color","blue");
+          $('.state'+count+' .regionRecovered').append(data[state].recovered).css("color","green");
+          $('.state'+count+' .regionDeath').append(data[state].deaths).css("color","red");
           if(count%3==0){
-            $('.container').append('<div class="row"><div class="state'+(count+1)+' col-md"><h3></h3><p class="regionConfirm"></p><p class="regionRecovered"></p><p class="regionDeath"></p></div><div class="state'+(count+2)+' col-md"><h3></h3><p class="regionConfirm"></p><p class="regionRecovered"></p><p class="regionDeath"></p></div><div class="state'+(count+3)+' col-md"><h3></h3><p class="regionConfirm"></p><p class="regionRecovered"></p><p class="regionDeath"></p></div></div>'); 
+            $('.container').append('<div class="row"><div class="state'+(count+1)+' col-md"><h3></h3><p>Confirmed Cases:&nbsp;<span class="regionConfirm"></span></p><p>Recovered:&nbsp;<span class="regionRecovered"></span></p><p>Deaths:&nbsp;<span class="regionDeath"></span></p></div><div class="state'+(count+2)+' col-md"><h3></h3><p>Confirmed Cases:&nbsp;<span class="regionConfirm"></span></p><p>Recovered:&nbsp;<span class="regionRecovered"></span></p><p>Deaths:&nbsp;<span class="regionDeath"></span></p></div><div class="state'+(count+3)+' col-md"><h3></h3><p>Confirmed Cases:&nbsp;<span class="regionConfirm"></span></p><p>Recovered:&nbsp;<span class="regionRecovered"></span></p><p>Deaths:&nbsp;<span class="regionDeath"></span></p></div></div>'); 
           }
           count++;
         }
